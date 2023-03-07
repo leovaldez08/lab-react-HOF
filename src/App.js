1218fec5fb68bf0d7bee035362926a457e701bab
Rishi Ganesh <116164199+leovaldez08@users.noreply.github.com>
2023-03-07 10:17:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HigherOrderComponent from "./components/HigherOrderComponent";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <HigherOrderComponent />
+    </div>
+  );
+}
+
+export default App;
